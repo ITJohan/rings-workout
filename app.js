d88f51fdbf4de63da2b8a34feed733ed21e447cc
Johan Lindkvist <johanlindkvist89@gmail.com>
2022-04-26 19:59:21 +0200

    save workout to local storage

--- app.js
+++ app.js
@@ -2,7 +2,23 @@ const form = document.querySelector('form');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const formData = new FormData(e.target);
-  console.log(formData);
-  const formProps = Object.fromEntries(formData);
-  console.log(formProps);
+  const workout = Object.fromEntries(formData);
+
+  workout.date = Date.now();
+  workout.type = 'A';
+
+  saveWorkout(workout);
 });
+
+const saveWorkout = (workout) => {
+  const workouts = getWorkouts();
+
+  if (workouts) {
+    const updatedWorkouts = [...JSON.parse(workouts), workout];
+    localStorage.setItem('workouts', JSON.stringify(updatedWorkouts));
+  } else {
+    localStorage.setItem('workouts', JSON.stringify([workout]));
+  }
+};
+
+const getWorkouts = () => localStorage.getItem('workouts');
