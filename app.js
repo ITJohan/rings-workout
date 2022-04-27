5c03d66c14e16f5c6d34c7c7b792745f8e934789
Johan Lindkvist <johanlindkvist89@gmail.com>
2022-04-27 20:37:00 +0200

    almost there with PoC

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
+import { clearForm, renderPreviousWorkoutInForm } from './renderUtils.js';
 import { pipe } from './utils.js';
-import { addWorkoutDate, addWorkoutType, postWorkout } from './workoutUtils.js';
+import {
+  addPullLevel,
+  addPushLevel,
+  addWorkoutDate,
+  addWorkoutType,
+  postWorkout,
+} from './workoutUtils.js';
 
 const form = document.querySelector('form');
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const formData = new FormData(e.target);
   const workout = Object.fromEntries(formData);
 
-  pipe(addWorkoutDate, addWorkoutType, postWorkout)(workout);
+  pipe(
+    addWorkoutDate,
+    addWorkoutType,
+    addPushLevel,
+    addPullLevel,
+    postWorkout
+  )(workout);
+
+  clearForm();
 });
+
+renderPreviousWorkoutInForm();
