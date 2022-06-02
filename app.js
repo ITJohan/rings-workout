b65b89558a6893ea33e97f54158f9cb421ccf5f4
Johan Lindkvist <johanlindkvist89@gmail.com>
2022-06-02 21:14:24 +0200

    clear logic and add basic add

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-import { clearForm, renderPreviousWorkoutInForm } from './renderUtils.js';
-import { pipe } from './utils.js';
-import {
-  addPullLevel,
-  addPushLevel,
-  addWorkoutDate,
-  addWorkoutType,
-  postWorkout,
-} from './workoutUtils.js';
-
 const form = document.querySelector('form');
 
 form.addEventListener('submit', (e) => {
@@ -15,15 +5,32 @@ form.addEventListener('submit', (e) => {
   const formData = new FormData(e.target);
   const workout = Object.fromEntries(formData);
 
-  pipe(
-    addWorkoutDate,
-    addWorkoutType,
-    addPushLevel,
-    addPullLevel,
-    postWorkout
-  )(workout);
+  workout.date = new Date();
 
-  clearForm();
+  const workouts = JSON.parse(localStorage.getItem('workouts') ?? '[]');
+
+  workouts.push(workout);
+
+  localStorage.setItem('workouts', JSON.stringify(workouts));
+
+  generateWorkout();
 });
 
-renderPreviousWorkoutInForm();
+const generateWorkout = () => {
+  const workouts = JSON.parse(localStorage.getItem('workouts') ?? '[]');
+
+  if (workouts.length > 1) {
+    console.log('a');
+    const previousWorkout = workouts[workouts.length - 2];
+
+    for (const [attribute, value] of Object.entries(previousWorkout)) {
+      if (attribute === 'date') continue;
+      document.getElementById(attribute).value = value;
+    }
+  } else if (workouts.length > 0) {
+    console.log('b');
+    form.setAttribute('value', 'B');
+  }
+};
+
+generateWorkout();
